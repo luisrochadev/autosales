6e802bf72778ed9fa59841039e1082dcfa3a65ff
LUIS ROCHA <luisrocha1993@gmail.com>
2016-03-24 21:48:20 -0700

    added imagesLoaded plugin for more stable hover effect

--- js/init.js
+++ js/init.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
 // SLIDING MENU INITIALIZATION	
   $('#simple-menu').sidr();
 
-// HOVER DETAILS INITIALIZATION
+
+});
+
+
+$('#container').imagesLoaded( function() {
+	// HOVER DETAILS INITIALIZATION
   $('#h1, #h2, #a1, #a2, #c1, #c2, #f1, #f2').contenthover({
     overlay_background:'#3d4852',
     overlay_opacity:0.8
